34290528b5453a48d52df7b8c0ab445eff558eb6
Matt Fisher <matt.fisherrr@gmail.com>
2019-06-20 15:36:45 -0600

    Changed default route

--- files/routes/__slash__.py
+++ files/routes/__slash__.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template, send_from_directory, current_app as ap
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    if path == 'favicon.ico':
-        return send_from_directory('build/',path)
-    return render_template('index.html')
+    return "<a href='https://Jeopardy.MattFisher.ca'>Jeopardy</a>"
+
+# def index(path):
+#     if path == 'favicon.ico':
+#         return send_from_directory('build/',path)
+#     return render_template('index.html')
