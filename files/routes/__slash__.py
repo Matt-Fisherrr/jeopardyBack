1eef630ecf8b66da2538ee4cf262013bc3d2d5c9
Matt Fisher <matt.fisherrr@gmail.com>
2019-06-25 11:33:03 -0600

    Heroku

--- files/routes/__slash__.py
+++ files/routes/__slash__.py
@@ -1,10 +1,5 @@
-from flask import Flask, render_template, send_from_directory, current_app as app
+from flask import Flask, current_app as app
 
 @app.route('/')
 def index():
     return "<a href='https://Jeopardy.MattFisher.ca'>Jeopardy</a>"
-
-# def index(path):
-#     if path == 'favicon.ico':
-#         return send_from_directory('build/',path)
-#     return render_template('index.html')
